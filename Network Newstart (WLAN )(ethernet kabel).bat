edec52744233587c835b91e28a031e7fe7cfb20a
Gigagamer9393 <alexander.huber93@web.de>
2024-11-27 21:11:06 +0100

    Network Newstart (WLAN )(ethernet kabel).bat

--- Network Newstart (WLAN )(ethernet kabel).bat
+++ Network Newstart (WLAN )(ethernet kabel).bat
@@ -1,7 +1,6 @@
-@echo off
-color 4
-title Newstart
-CLS
+@echo off 
+ color 4
+ CLS
  ECHO.
  ECHO =============================
  ECHO please wait
@@ -49,18 +48,94 @@ CLS
 :gotPrivileges
  setlocal & cd /d %~dp0
  if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
-echo   ********  **                                                           **       ******* 
-echo  **//////**//   *****                                                   /**      /**////**
-echo **      //  ** **///**  ******   ******  ******   ******  ******  ******/**      /**   /**
-echo/**         /**/**  /** //////** //**//* //////** ////**  **////**//**//*/**      /******* 
-echo/**    *****/**//******  *******  /** /   *******    **  /**   /** /** / /**      /**////  
-echo//**  ////**/** /////** **////**  /**    **////**   **   /**   /** /**   /**      /**      
-echo //******** /**  ***** //********/***   //******** ******//****** /***   /********/**      
-echo  ////////  //  /////   //////// ///     //////// //////  //////  ///    //////// //   
-time/t
-date/t
-@ping -n 5localhost> nu
-echo newstart 10sec 
-@ping -n 10localhost> nul
-timeout 10 
-shutdown /r /t 0
+
+title : Network newstart
+netsh interface set interface "Ethernet" disable
+netsh interface set interface "wlan" disable
+timeout 5
+netsh interface set interface "Ethernet" enable
+netsh interface set interface "WLAN" enable
+echo ----------------------------------
+echo Progress: [##------------------] 10%%
+echo ----------------------------------
+timeout 2 >nul
+cls
+echo.
+echo.
+echo
+echo ----------------------------------
+echo Progress: [####----------------] 20%%
+echo ----------------------------------
+timeout 1 >nul
+cls
+echo.
+echo.
+echo 
+echo ----------------------------------
+echo Progress: [######--------------] 30%%
+echo ----------------------------------
+timeout 2 >nul
+cls
+echo.
+echo.
+echo
+echo ----------------------------------
+echo Progress: [########------------] 40%%
+echo ----------------------------------
+timeout 1 >nul
+cls
+echo.
+echo.
+echo
+echo ----------------------------------
+echo Progress: [##########----------] 50%%
+echo ----------------------------------
+timeout 2 >nul
+cls
+echo.
+echo.
+echo
+echo ----------------------------------
+echo Progress: [############--------] 60%%
+echo ----------------------------------
+timeout 1 >nul
+cls
+echo.
+echo.
+echo
+echo ----------------------------------
+echo Progress: [##############------] 70%%
+echo ----------------------------------
+timeout 2 >nul
+cls
+echo.
+echo.
+echo 
+echo ----------------------------------
+echo Progress: [################----] 80%%
+echo ----------------------------------
+timeout 1 >nul
+cls
+echo.
+echo.
+echo
+echo ----------------------------------
+echo Progress: [##################--] 90%%
+echo ----------------------------------
+timeout 2 >nul
+cls
+echo.
+echo.
+echo
+echo ----------------------------------
+echo Progress: [####################] 100%%
+echo ----------------------------------
+timeout 10
+echo successfully
+exit
+
+
+
+
+
+
