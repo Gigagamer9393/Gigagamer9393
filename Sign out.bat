fdf71e28839cc4849d43748b782ad9728675d8ee
Gigagamer9393 <alexander.huber93@web.de>
2024-11-30 17:04:10 +0100

    Sign out.bat

--- Sign out.bat
+++ Sign out.bat
@@ -1,6 +1,6 @@
 @echo off
 Color 4
-title sign out
+title exit
 CLS
  ECHO.
  ECHO =============================
@@ -59,8 +59,69 @@ echo //******** /**  ***** //********/***   //******** ******//****** /***   /**
 echo  ////////  //  /////   //////// ///     //////// //////  //////  ///    //////// //   
 time/t
 date/t
-@ping -n 5localhost> nul
-echo log out 10sec 
-@ping -n 10localhost> nul
+echo shutown 10sec 
 timeout 10 
-shutdown -l
+shutdown /s /t 000@echo off
+Color 4
+title exit
+CLS
+ ECHO.
+ ECHO =============================
+ ECHO please wait
+ ECHO =============================
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~dpnx0"
+ rem this works also from cmd shell, other than %~0
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+  
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+echo   ********  **                                                           **       ******* 
+echo  **//////**//   *****                                                   /**      /**////**
+echo **      //  ** **///**  ******   ******  ******   ******  ******  ******/**      /**   /**
+echo/**         /**/**  /** //////** //**//* //////** ////**  **////**//**//*/**      /******* 
+echo/**    *****/**//******  *******  /** /   *******    **  /**   /** /** / /**      /**////  
+echo//**  ////**/** /////** **////**  /**    **////**   **   /**   /** /**   /**      /**      
+echo //******** /**  ***** //********/***   //******** ******//****** /***   /********/**      
+echo  ////////  //  /////   //////// ///     //////// //////  //////  ///    //////// //   
+time/t
+date/t
+echo shutown 10sec 
+timeout 10 
+shutdown /s /t 000
